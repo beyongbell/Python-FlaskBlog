d36fd634f859c47309d0fdd18864a6aa9a7e86ee
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-09-27 13:04:58 +0700

    Python Flask

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-flask_blog = Flask(__name__)
-flask_blog.config['SECRET_KEY'] = 'd42f7b60fad9c6c81f2d3c4b48650844'
-flask_blog.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(flask_blog)
-
-from app import routes
